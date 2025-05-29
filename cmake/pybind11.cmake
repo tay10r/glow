f94be9632b3f4b9f944f30a8eabf01d4ae866e41
Taylor Holberton <taylor.holberton@proton.me>
2025-05-29 10:10:35 -0400

    Add FetchContent policy setting

--- cmake/pybind11.cmake
+++ cmake/pybind11.cmake
@@ -1,5 +1,9 @@
 include_guard()
 
+if(POLICY CMP0135)
+  cmake_policy(SET CMP0135 NEW)
+endif()
+
 include(FetchContent)
 
 FetchContent_Declare(pybind11 URL "https://github.com/pybind/pybind11/archive/refs/tags/v2.12.0.zip")
